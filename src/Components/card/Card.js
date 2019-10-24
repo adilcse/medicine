19836f3f9d56e0e54afb9d8bb2c4bee72e4e7ebe
unknown <adildilhussain02@gmail.com>
2019-10-24 14:06:05 +0530

    card changed

--- src/Components/card/Card.js
+++ src/Components/card/Card.js
@@ -1,30 +1,24 @@
 import React from 'react';
+import { Card as Cardboot ,Button} from 'react-bootstrap';
 import './Card.css';
-import  {  Link } from "react-router-dom";
 function Card({source}){
   
 return(
- 
-<div className="card mb-3 cl1 bd" >
-  <div className="row no-gutters">
-   
-    <div className="col-md-4">
-      <img src={source.imageurl} className="card-img" alt={source.name}/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body" align="left">
-      <Link to={`/Product/${source.item_id}`}>  <h4 className="card-title">{source.name}<br/>
-         {source.price} ₹ only   </h4> </Link> <h5>10 % off</h5>
-       
-    
-        <p className="card-text">{source.description}</p>
-       
-      </div>
-    </div>
-   
-  </div>
-</div>
 
+
+
+<Cardboot style={{ width: '18rem' }} >
+  <Cardboot.Img variant="top" src={source.imageurl} className="card-img zoom" />
+  <Cardboot.Body>
+    <Cardboot.Title><div className="title">{source.name}</div></Cardboot.Title>
+    <Cardboot.Text>
+   <h3><i><small><strike>MRP ₹{Math.floor(parseInt(source.price)*1.1)}</strike></small> </i>₹{source.price}</h3>
+    </Cardboot.Text>
+    <Button variant="primary">Buy </Button> 
+    <Button variant="primary">Add to Cart</Button> 
+  </Cardboot.Body>
+</Cardboot>
+  
 );
 }
 export default Card;
