f10b4740515a210d6848ed64dd18b1ccaf87bce9
adilcse <48638728+adilcse@users.noreply.github.com>
2020-01-10 10:52:45 +0530

    Update index.jsx

--- src/firebaseconnect/index.jsx
+++ src/firebaseconnect/index.jsx
@@ -1,14 +1,7 @@
 import * as firebase from "firebase";
 import "firebase/auth";
 var firebaseConfig = {
-  apiKey: "AIzaSyDTZsKTmbcRjGwNF652wIdTv_rkiWkQZQQ",
-  authDomain: "revive-medicine.firebaseapp.com",
-  databaseURL: "https://revive-medicine.firebaseio.com",
-  projectId: "revive-medicine",
-  storageBucket: "revive-medicine.appspot.com",
-  messagingSenderId: "605018565755",
-  appId: "1:605018565755:web:326651baa13394719bc6de",
-  measurementId: "G-GK6EMMGWE7"
+//paste firebase config here
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
